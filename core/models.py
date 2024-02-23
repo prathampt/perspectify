8efbdd68df6760f06fec6d3602db2ffabd9bf01c
prathampt <prathamt12345@gmail.com>
2024-02-23 20:10:37 +0530

    Added new userfields, linking with signals, and fix profile fetching

--- core/models.py
+++ core/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    place = models.CharField(max_length=100)
+    interests = models.CharField(max_length=200)
+    about_me = models.TextField()
+    language = models.CharField(max_length=100)
 
-
+    def __str__(self):
+        return self.user.username
